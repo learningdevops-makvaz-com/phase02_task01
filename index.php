3cf67cd58036f39a53ba68f8776234f7de9f5da9
Elliot <ellioteb@gmail.com>
2021-06-08 22:05:56 +0100

    indentation

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'hostname='.gethostname().'</br>';
 if (getenv('VERSION') == null) {
-echo '$VERSION not set';
+    echo '$VERSION not set';
 } else {
     echo "$VERSION=".getenv('VERSION');
 }
